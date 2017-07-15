18ad2c2d1798b2d60aa7e710dbc120afc8a96c9c
Gennady Tanchin <g.tanchin@yandex.ru>
2017-07-15 18:43:40 +0300

    ЭюЭ

--- Debug/src/my_function.d
+++ Debug/src/my_function.d
@@ -1,127 +1,127 @@
 src/my_function.o: ../src/my_function.c \
- /home/jet/work/workspace/expedition/system/inc/cmsis/stm32f0xx.h \
- /home/jet/work/workspace/expedition/system/inc/cmsis/stm32f030x6.h \
- /home/jet/work/workspace/expedition/system/inc/cmsis/core_cm0.h \
- /home/jet/work/workspace/expedition/system/inc/cmsis/core_cmInstr.h \
- /home/jet/work/workspace/expedition/system/inc/cmsis/core_cmFunc.h \
- /home/jet/work/workspace/expedition/system/inc/cmsis/system_stm32f0xx.h \
- /home/jet/work/workspace/expedition/inc/stm32f0xx_conf.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_dma.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_exti.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_gpio.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_rcc.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_rtc.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_spi.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_tim.h \
- /home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_usart.h \
- /home/jet/work/workspace/expedition/inc/my_main.h \
- /home/jet/work/workspace/expedition/inc/my_bt01_def.h \
- /home/jet/work/workspace/expedition/inc/deviceid.h \
- /home/jet/work/workspace/expedition/inc/my_service.h \
- /home/jet/work/workspace/expedition/inc/cube_hal.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_server.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/compiler.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/ble_status.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gap.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/link_layer.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/ble_status.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gap_aci.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/events.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_aci.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_server.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/hci_const.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/clock.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/link_layer.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/gp_timer.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_hal_aci.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_aci_const.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/hci_const.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/hci.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/list.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/sm.h \
- /home/jet/work/workspace/expedition/bluenrg/inc/debug.h \
- /home/jet/work/workspace/expedition/inc/my_service.h
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/stm32f0xx.h \
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/stm32f030x6.h \
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cm0.h \
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cmInstr.h \
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cmFunc.h \
+ /home/jet/workspace/pak.exp-2/system/inc/cmsis/system_stm32f0xx.h \
+ /home/jet/workspace/pak.exp-2/inc/stm32f0xx_conf.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_dma.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_exti.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_gpio.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_rcc.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_rtc.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_spi.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_tim.h \
+ /home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_usart.h \
+ /home/jet/workspace/pak.exp-2/inc/my_main.h \
+ /home/jet/workspace/pak.exp-2/inc/my_bt01_def.h \
+ /home/jet/workspace/pak.exp-2/inc/deviceid.h \
+ /home/jet/workspace/pak.exp-2/inc/my_service.h \
+ /home/jet/workspace/pak.exp-2/inc/cube_hal.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_server.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/compiler.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/ble_status.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gap.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/link_layer.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/ble_status.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gap_aci.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/events.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_aci.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_server.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/hci_const.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/clock.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/link_layer.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/gp_timer.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_hal_aci.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_aci_const.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/hci_const.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/hci.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/list.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/sm.h \
+ /home/jet/workspace/pak.exp-2/bluenrg/inc/debug.h \
+ /home/jet/workspace/pak.exp-2/inc/my_service.h
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/stm32f0xx.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/stm32f0xx.h:
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/stm32f030x6.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/stm32f030x6.h:
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/core_cm0.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cm0.h:
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/core_cmInstr.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cmInstr.h:
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/core_cmFunc.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/core_cmFunc.h:
 
-/home/jet/work/workspace/expedition/system/inc/cmsis/system_stm32f0xx.h:
+/home/jet/workspace/pak.exp-2/system/inc/cmsis/system_stm32f0xx.h:
 
-/home/jet/work/workspace/expedition/inc/stm32f0xx_conf.h:
+/home/jet/workspace/pak.exp-2/inc/stm32f0xx_conf.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_dma.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_dma.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_exti.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_exti.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_gpio.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_gpio.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_rcc.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_rcc.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_rtc.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_rtc.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_spi.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_spi.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_tim.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_tim.h:
 
-/home/jet/work/workspace/expedition/system/inc/stm32f0-stdperiph/stm32f0xx_usart.h:
+/home/jet/workspace/pak.exp-2/system/inc/stm32f0-stdperiph/stm32f0xx_usart.h:
 
-/home/jet/work/workspace/expedition/inc/my_main.h:
+/home/jet/workspace/pak.exp-2/inc/my_main.h:
 
-/home/jet/work/workspace/expedition/inc/my_bt01_def.h:
+/home/jet/workspace/pak.exp-2/inc/my_bt01_def.h:
 
-/home/jet/work/workspace/expedition/inc/deviceid.h:
+/home/jet/workspace/pak.exp-2/inc/deviceid.h:
 
-/home/jet/work/workspace/expedition/inc/my_service.h:
+/home/jet/workspace/pak.exp-2/inc/my_service.h:
 
-/home/jet/work/workspace/expedition/inc/cube_hal.h:
+/home/jet/workspace/pak.exp-2/inc/cube_hal.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_server.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_server.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/compiler.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/compiler.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/ble_status.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/ble_status.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gap.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gap.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/link_layer.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/link_layer.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/ble_status.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/ble_status.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gap_aci.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gap_aci.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/events.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/events.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_aci.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_aci.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_gatt_server.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_gatt_server.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/hci_const.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/hci_const.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/clock.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/clock.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/link_layer.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/link_layer.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/gp_timer.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/gp_timer.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_hal_aci.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_hal_aci.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/bluenrg_aci_const.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/bluenrg_aci_const.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/hci_const.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/hci_const.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/hci.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/hci.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/list.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/list.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/sm.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/sm.h:
 
-/home/jet/work/workspace/expedition/bluenrg/inc/debug.h:
+/home/jet/workspace/pak.exp-2/bluenrg/inc/debug.h:
 
-/home/jet/work/workspace/expedition/inc/my_service.h:
+/home/jet/workspace/pak.exp-2/inc/my_service.h:
